4b3672c841c54ee37609235a090d9ae0b8995370
serhii <proektssd@gmail.com>
2023-10-22 16:50:01 +0300

    docs(mixins): add docstings to mixin class

--- dreamteam_project/api/API/mixins.py
+++ dreamteam_project/api/API/mixins.py
@@ -1,8 +1,23 @@
+"""
+The module represents CUSTOM MIXIN class used in the project.
+"""
+
 from ..models import Member
 
 
 class MemberMixin:
+    """
+    Mixin for different Member views that provides
+    a pre-defined queryset for Member model.
+    """
+
     def get_queryset(self):
+        """
+        Gets a queryset for Member model with selected fields
+        and team memberships prefetched.
+
+        :return: A queryset for Member model.
+        """
         return Member.objects.only(
             'username',
             'first_name',
